3eab02e65419205fd82efc51f000b43dac16389e
kvsrinivas91 <kvsrinivas91@gmail.com>
2019-05-04 19:04:28 +0530

    Added changes

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
-from sqlalchemy.dialects.postgresql import HSTORE
-from sqlalchemy.ext.mutable import MutableDict
+#from sqlalchemy.dialects.postgresql import HSTORE
+#from sqlalchemy.ext.mutable import MutableDict
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -18,22 +19,24 @@ class Pokemon(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     sprite = db.Column(db.String(150))
-    #cardColours = db.Column(db.String(120))
-    cardColours=db.Column(MutableDict.as_mutable(HSTORE))
+    cardColours = db.Column(db.String(120))
+    #cardColours=db.Column(MutableDict.as_mutable(HSTORE))
 
-    def __init__(self, name, sprite, cardColours):
+    def __init__(self, name, sprite,fg,bg,desc):
         self.name=name
         self.sprite=sprite
-        self.cardColours=cardColours
+        self.fg=fg
+        self.bg=bg
+        self.desc=desc
 
 #Pokemon Schema
-class PokemonSchema(ma.Schema):
+'''class PokemonSchema(ma.Schema):
     class Meta:
         # Fields to display
-        fields=('id','name','sprite','cardColours')
+        fields=('id','name','sprite','cardColours')'''
 
 #Init Schema
-Pokemon_Schema=PokemonSchema()
+#Pokemon_Schema=PokemonSchema()
 
 #create a new Pokemon
 @app.route("/api/pokemon/", methods=["POST"])
@@ -43,12 +46,19 @@ def create_Pokemon():
     name=pokemon['name']
     sprite=pokemon['sprite']
     cardColours=pokemon['cardColours']
-    new_pokemon = Pokemon(name, sprite, cardColours)    
+    fg=pokemon['cardColours']['fg']
+    bg=pokemon['cardColours']['bg']
+    desc=pokemon['cardColours']['desc']
+    
+    new_pokemon=Pokemon(name,sprite,fg,bg,desc)
 
     db.session.add(new_pokemon)
     db.session.commit()
    
-    return Pokemon_Schema.jsonify({"pokemon":new_pokemon})
+    pokemon=Pokemon.query.filter(Pokemon.name==name).first()
+    new_pokemon={'pokemon' : {'id' : pokemon.id, 'name' : pokemon.name, 'sprite' : pokemon.sprite, 'cardColours' : {'fg' : pokemon.fg, 'bg' : pokemon.bg, 'desc' : pokemon.desc}}}
+    return jsonify(new_pokemon)
+    #return Pokemon_Schema.jsonify({"pokemon":new_pokemon})
     #return Pokemon_Schema.jsonify(new_pokemon)
  
 #Run server 
